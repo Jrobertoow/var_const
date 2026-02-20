2fdb5b4b0492835891db997bc80e82e9a4e78054
norasantoss124 <norasantoss124@gmail.com>
2026-02-20 16:18:58 -0400

    Aula var e const

---
+++ var_const.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Const_var</title>
+</head>
+<body>
+    <?php 
+    /*var aluno = "nome";
+    int = numero,
+    string = caractere,
+    boleano = true/false,
+    float = 0.0 */
+    
+    $aluno = "Arnoldo shuaseneguer";
+    $idade = "130 anos";
+    //const intituicao = "cetam";
+    define("instituicao", "centro de educacao tec. do amazonas");
+    
+    echo "Olá aluno, $aluno $idade bem vindo ao " . instituicao
+    
+    
+    ?>
+</body>
+</html>
